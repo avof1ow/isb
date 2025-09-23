4c5d1f21ca7fa5ac54246795fadde87431cdb582
avof1ow <132481548+avof1ow@users.noreply.github.com>
2025-09-23 17:20:14 +0300

    Update tester.py

--- ISB_LAB_2/tester.py
+++ ISB_LAB_2/tester.py
@@ -1,32 +1,22 @@
-import os
-from typing import Optional
 from tests import frequency_test, runs_test, longest_run_test
 
-def test_sequence(filename: str) -> Optional[None]:
+
+def test_sequence(filename: str) -> tuple[float, float, float]:
     """
     Проводит серию тестов для двоичной последовательности.
 
     Args:
         filename: Путь к файлу, содержащему тестируемую двоичную последовательность.
+    Return:
+        tuple(float, float, float): Кортеж, состоящий из результатов тестов
     """
-    if not os.path.exists(filename):
-        print(f"Ошибка: Не удалось открыть файл {filename}!")
-        return
 
     with open(filename, 'r') as f:
         sequence = f.readline().strip()
-
-    print(f"\nТестирование последовательности из файла {filename}:")
-    print(f"Последовательность: {sequence}")
-    print(f"Длина: {len(sequence)}")
 
     # Выполнение тестов
     p1 = frequency_test(sequence)
     p2 = runs_test(sequence)
     p3 = longest_run_test(sequence)
 
-    # Вывод результатов
-    print(f"\nРезультаты тестов для {filename}:")
-    print(f"Частотный тест p-значение: {p1:.6f} {'(Passed)' if p1 >= 0.01 else '(Failed)'}")
-    print(f"Тест на серии p-значение: {p2:.6f} {'(Passed)' if p2 >= 0.01 else '(Failed)'}")
-    print(f"Тест на самую длинную серию p-значение: {p3:.6f} {'(Passed)' if p3 >= 0.01 else '(Failed)'}")
+    return p1, p2, p3
