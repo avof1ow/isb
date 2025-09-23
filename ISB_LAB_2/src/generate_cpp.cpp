d8d0fc2902938018e74d14ced7110776fa4a9ff3
avof1ow <132481548+avof1ow@users.noreply.github.com>
2025-09-23 17:18:18 +0300

    Update generate_cpp.cpp

--- ISB_LAB_2/src/generate_cpp.cpp
+++ ISB_LAB_2/src/generate_cpp.cpp
@@ -1,67 +1,39 @@
 #include <iostream>
-#include <random>
-#include <bitset>
 #include <fstream>
 #include <string>
 
 using namespace std;
 
 string getInputFileCpp(const string& filename) {
-	// парсер для извлечения значения поля input_file_cpp из settings.json
     ifstream file(filename);
     if (!file.is_open()) {
         cerr << "Error: Could not open " << filename << "!" << endl;
         return "";
     }
 
-    string content((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
+    string line;
+    while (getline(file, line)) {
+        // Удаляем пробелы и табы для упрощения поиска
+        string cleaned_line;
+        for (char c : line) {
+            if (c != ' ' && c != '\t') {
+                cleaned_line += c;
+            }
+        }
+
+        // Ищем ключ input_file_cpp
+        size_t pos = cleaned_line.find("\"input_file_cpp\":\"");
+        if (pos != string::npos) {
+            size_t start = pos + 16; // Длина "\"input_file_cpp\":\""
+            size_t end = cleaned_line.find("\"", start);
+            if (end != string::npos) {
+                file.close();
+                return cleaned_line.substr(start, end - start);
+            }
+        }
+    }
+
     file.close();
-
-    // Ищем "input_file_cpp": "путь"
-    string key = "\"input_file_cpp\": \"";
-    size_t start = content.find(key);
-    if (start == string::npos) {
-        cerr << "Error: 'input_file_cpp' not found in " << filename << "!" << endl;
-        return "";
-    }
-
-    start += key.length();
-    size_t end = content.find("\"", start);
-    if (end == string::npos) {
-        cerr << "Error: Invalid format of 'input_file_cpp' in " << filename << "!" << endl;
-        return "";
-    }
-
-    return content.substr(start, end - start);
-}
-
-void generateRandomSequenceCpp(const string& output_file) {
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<> dis(0, 1);
-	
-    bitset<128> sequence;
-    for (int i = 0; i < 128; ++i) {
-        sequence[i] = dis(gen);
-    }
-	
-    cout << "C++ Random Sequence: " << sequence << endl;
-	
-    ofstream out(output_file);
-    if (out.is_open()) {
-        out << sequence;
-        out.close();
-        cout << "Sequence saved to " << output_file << endl;
-    } else {
-        cerr << "Error: Could not save sequence to " << output_file << "!" << endl;
-    }
-}
-
-int main() {
-    string output_file = getInputFileCpp("settings.json");
-    if (output_file.empty()) {
-        return 1;
-    }
-    generateRandomSequenceCpp(output_file);
-    return 0;
+    cerr << "Error: 'input_file_cpp' not found or has invalid format in " << filename << "!" << endl;
+    return "";
 }
