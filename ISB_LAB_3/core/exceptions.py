8a9580d75e0b4f5057229628914ab1dc098cd499
avof1ow <132481548+avof1ow@users.noreply.github.com>
2025-09-23 16:59:29 +0300

    Update exceptions.py

--- ISB_LAB_3/core/exceptions.py
+++ ISB_LAB_3/core/exceptions.py
@@ -1,18 +1,23 @@
 class CryptoError(Exception):
     """Базовое исключение для всех ошибок криптографических операций."""
+    pass
 
 
 class KeyGenerationError(CryptoError):
     """Исключение при генерации ключей."""
+    pass
 
 
 class EncryptionError(CryptoError):
     """Исключение при шифровании данных."""
+    pass
 
 
 class DecryptionError(CryptoError):
     """Исключение при дешифровании данных."""
+    pass
 
 
 class FileOperationError(CryptoError):
     """Исключение при операциях с файлами (чтение/запись)."""
+    pass
