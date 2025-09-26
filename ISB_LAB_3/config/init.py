9c969b2d15f84720096a28daba7e4e180822fc15
avof1ow <132481548+avof1ow@users.noreply.github.com>
2025-09-27 00:24:57 +0400

    Delete ISB_LAB_3/config/init.py

--- ISB_LAB_3/config/init.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-
-# Константы для путей
-CONFIG_DIR = Path(__file__).parent  # Директория с конфигурацией (utils/)
-PROJECT_ROOT = CONFIG_DIR.parent    # Корень проекта (папка выше utils/)
