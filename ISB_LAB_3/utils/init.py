82b229676384cc588d1854b013768cd25d1dee8e
avof1ow <132481548+avof1ow@users.noreply.github.com>
2025-09-27 00:25:26 +0400

    Delete ISB_LAB_3/utils/init.py

--- ISB_LAB_3/utils/init.py
+++
@@ -1,8 +0,0 @@
-"""
-Пакет utils - вспомогательные утилиты приложения.
-"""
-
-from .config_manager import ConfigManager
-from .file_io import read_file, write_file
-
-__all__ = ["ConfigManager", "read_file", "write_file"]
